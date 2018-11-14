e5cb7cc51287397cd29899246f6e56f8fc055af9
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-14 11:03:47 -0500

    update

--- ConvertingTemps(BenM)/ConvertingTemps(BenM)/ConvertingTempForm.Designer.cs
+++ ConvertingTemps(BenM)/ConvertingTemps(BenM)/ConvertingTempForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.lblEnterTemp = new System.Windows.Forms.Label();
             this.txtTempCel = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblEnterTemp
@@ -49,15 +50,28 @@
             this.txtTempCel.Size = new System.Drawing.Size(100, 20);
             this.txtTempCel.TabIndex = 1;
             // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(124, 154);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(192, 61);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert to Farenheight";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
             // frmConvertingTemp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 347);
+            this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.txtTempCel);
             this.Controls.Add(this.lblEnterTemp);
             this.Name = "frmConvertingTemp";
             this.Text = "Temp Converter";
+            this.Load += new System.EventHandler(this.frmConvertingTemp_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +81,7 @@
 
         private System.Windows.Forms.Label lblEnterTemp;
         private System.Windows.Forms.TextBox txtTempCel;
+        private System.Windows.Forms.Button btnConvert;
     }
 }
 
--- ConvertingTemps(BenM)/ConvertingTemps(BenM)/ConvertingTempForm.cs
+++ ConvertingTemps(BenM)/ConvertingTemps(BenM)/ConvertingTempForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Ben Motz
+ * Created on: 14-11-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Temp Converter
+ * This program converts celcius to farenheight
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,34 @@ namespace ConvertingTemps_BenM_
         public frmConvertingTemp()
         {
             InitializeComponent();
+        }
+        //Creates ConvertToF procedure
+        public void ConvertToF(int celcius)
+        {
+            //Creates Farenheight variable
+            double Farenheight;
+
+            //Calculation for farenheight
+           Farenheight = (double) (9) / (double) (5) * celcius + 32;
+
+            //Displays farenheight in message
+            MessageBox.Show("The temperature is: " + Farenheight + " degrees farenheight.");
+        }
+
+        private void frmConvertingTemp_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //Declares celcius as a variable
+            int userCelcius;
+
+            //Decalres what celcius is
+            userCelcius = Convert.ToInt16(this.txtTempCel.Text);
+
+            //Converts to fareneheight
+            this.ConvertToF(userCelcius);
         }
     }
 }
