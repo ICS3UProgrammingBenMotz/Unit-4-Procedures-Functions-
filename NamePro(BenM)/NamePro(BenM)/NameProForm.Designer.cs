141da65f013d4a0a46d0424b65c3939dcfba7897
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2018-11-21 09:56:26 -0500

    address

---
+++ NamePro(BenM)/NamePro(BenM)/NameProForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace NamePro_BenM_
+{
+    partial class frmNamePro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblMidName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnPressMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(64, 43);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(85, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "Enter First Name";
+            // 
+            // lblMidName
+            // 
+            this.lblMidName.AutoSize = true;
+            this.lblMidName.Location = new System.Drawing.Point(63, 120);
+            this.lblMidName.Name = "lblMidName";
+            this.lblMidName.Size = new System.Drawing.Size(97, 13);
+            this.lblMidName.TabIndex = 1;
+            this.lblMidName.Text = "Enter Middle Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(63, 205);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(86, 13);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Enter Last Name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(172, 40);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(172, 113);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(100, 20);
+            this.txtMiddleName.TabIndex = 4;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(172, 202);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // btnPressMe
+            // 
+            this.btnPressMe.Location = new System.Drawing.Point(355, 137);
+            this.btnPressMe.Name = "btnPressMe";
+            this.btnPressMe.Size = new System.Drawing.Size(134, 47);
+            this.btnPressMe.TabIndex = 6;
+            this.btnPressMe.Text = "Press Me!";
+            this.btnPressMe.UseVisualStyleBackColor = true;
+            this.btnPressMe.Click += new System.EventHandler(this.btnPressMe_Click);
+            // 
+            // frmNamePro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 309);
+            this.Controls.Add(this.btnPressMe);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblMidName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "frmNamePro";
+            this.Text = "What is my name?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblMidName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnPressMe;
+    }
+}
+
